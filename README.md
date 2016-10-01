b68eaddc82c9558604dfb15a32b5a1e109288b9c
Bader <baderbuddy@gmail.com>
2016-09-30 20:37:15 -0400

    Added unit tests, updated dependencies

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 [![travis](http://img.shields.io/travis/deepsweet/mustache-loader.svg?style=flat-square)](https://travis-ci.org/deepsweet/mustache-loader)
 [![climate](http://img.shields.io/codeclimate/github/deepsweet/mustache-loader.svg?style=flat-square)](https://codeclimate.com/github/deepsweet/mustache-loader/code)
 [![peer deps](http://img.shields.io/david/peer/deepsweet/mustache-loader.svg?style=flat-square)](https://david-dm.org/deepsweet/mustache-loader#info=peerDependencies)
+[![dependencies](http://img.shields.io/david/peer/deepsweet/mustache-loader.svg?style=flat-square)](https://david-dm.org/deepsweet/mustache-loader#info=dependencies)
 
 Compiles [Mustache](https://mustache.github.io/) templates with [Hogan](https://twitter.github.io/hogan.js/) and optionally [html-minifier](https://github.com/kangax/html-minifier).
 
---
+++ test/index.js
@@ -0,0 +1,56 @@
+var expect = require("chai").expect;
+var mustacheLoader = require("../index");
+var fs = require("fs");
+describe("MustacheLoader", function () {
+    describe("Basic run", function () {
+        it("runs without errors", function (done) {
+            fs.readFile('test/sample.html', 'utf8', function (err, data) {
+                try {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+                    var results = mustacheLoader.apply({}, [data]);
+                    var module = {};
+                    eval(results);
+                    expect(module.exports).to.not.be.undefined;
+                    expect(module.exports).to.be.a("function");
+                    var templateResults = module.exports({ a: 5 });
+                    expect(templateResults).to.be.equal("<html>\n\
+    <head></head>\n\
+    <body>\n\
+        <div>5</div>\n\
+    </body>\n\
+</html>")
+                    done();
+                }
+                catch (e) {
+                    done(e);
+                }
+            })
+        });
+    });
+    describe("Minify", function () {
+        it("runs quickly", function (done) {
+           fs.readFile('test/sample.html', 'utf8', function (err, data) {
+                try {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+                    var results = mustacheLoader.apply({query: { minify: true}}, [data]);
+                    var module = {};
+                    eval(results);
+                    expect(module.exports).to.not.be.undefined;
+                    expect(module.exports).to.be.a("function");
+                    var templateResults = module.exports({ a: 5 });
+                    expect(templateResults).to.be.equal("<html><head></head><body><div>5</div></body></html>")
+                    done();
+                }
+                catch (e) {
+                    done(e);
+                }
+            })
+        });
+    });
+});
